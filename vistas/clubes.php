4ab3be5df12faccb111e31a8c1ead2f84afa3034
crisdaniecm <crisdanielcm@hotmail.com>
2015-05-03 12:38:16 -0500

    arreglo

--- vistas/clubes.php
+++ vistas/clubes.php
@@ -1,91 +1,37 @@
 <div class="club" id="contenido">
-
-		<table BORDER="2" CELLSPACING="3" WIDTH=100%>
-			<tbody>
-				<tr>
-					<td>
-						<th>Club</th>
-					</td>
-					<td>
-						<th>Contacto</th>
-					</td>
-					<td>					
-						<th>Administrativo</th>
-					</td>
-					<td>
-						<th>Información Adicional</th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-				</tr>
-			</tbody>
-		</table>
+	<div class="container">
+		<div class="row">
+			<div class=" col-xs-12 col-md-10 col-md-offset-1 padding-left">
+				<div class="panel panel-default borde">
+					<div class="panel-heading">
+	    				<h1 class="panel-title">Clubes</h1>
+	  				</div>
+	  				<div class="panel-body">
+	  					<div class="table-responsive">
+	  						<table class="table">
+								<thead>
+									<tr>
+									<th>Club</th>
+									<th>Contacto</th>
+									<th>Administrativos</th>
+									<th>Información Adicional</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td style="width: 316px"><a class="img-thumbnail"><img src="" height="187" width="249" alt=""></a></td>
+										<td>7472235</td>
+										<td>Pedro, Presidente 
+											juan, contador
+										</td>
+										<td>correo@hotmail.com</td>
+									</tr>
+								</tbody>
+							</table>
+	  					</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
 </div>
--- vistas/organoAdm.php
+++ vistas/organoAdm.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class=" col-xs-12 col-md-10 col-md-offset-1 padding-left">
-				<div class="panel panel-default borde">
+				<div class="panel panel-default">
 					<div class="panel-heading">
 	    				<h1 class="panel-title">Organo Administrativo</h1>
 	  				</div>
