07d7f86fea0e8b04955428267ac90c1ecaaa814a
nia1124 <nia-5@hotmail.com>
2015-05-03 12:29:58 -0500

    paginas: eventos, club, ranking masculino

--- vistas/eventos.php
+++ vistas/eventos.php
@@ -1,127 +1,43 @@
+<div class="eventos" id="contenido">
+	<div class="container">
+		<div class="row">
+			<div class=" col-xs-12 col-md-10 col-md-offset-1 padding-left">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+	    				<h1 class="panel-title">Eventos</h1>
+	  				</div>
+	  				<div class="panel-body">
+	  					<div class="table-responsive">
+	  						<table class="table">
+								<thead>
+									<tr>
+									<th>N°</th>
+									<th>Campeonato / Actividad</th>
+									<th>Categoria</th>
+									<th>Modalidad</th>
+									<th>Ciudad</th>
+									<th>Fecha</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1</td>
+										<td>Abierto</td>
+										<td>Todas</td>
+										<td>Armenia</td>
+										<td>Enero 2015</td>
+										<td>Armenia</td>
+									</tr>
+								</tbody>
+							</table>
+	  					</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+</div>
 
-	<div class="eventos" id="contenido">
-		<table BORDER="2" CELLSPACING="3" WIDTH=100%>
-			<tbody>
-				<tr>
-					<td>
-						<th>N°</th>
-					</td>
-					<td>
-						<th>Campeonato / Actividad</th>
-					</td>
-					<td>					
-						<th>Categoría</th>
-					</td>
-					<td>
-						<th>Modalidad</th>
-					</td>
-					<td>
-						<th>Ciudad</th>
-					</td>
-					<td>					
-						<th>Fecha</th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>
-						<th></th>
-					</td>
-					<td>					
-						<th></th>
-					</td>
-				</tr>
-			</tbody>
-		</table>	
-	</div>	
+	
+
+				
--- vistas/rankingM.php
+++ vistas/rankingM.php
@@ -1,18 +1,38 @@
-
-	<div class="rankingM" id="contenido">
-		<table BORDER="2" CELLSPACING="3" WIDTH=100%>
-			<tbody>
-				<tr>
-					<td>
-						<th>N°</th>
-					</td>
-					<td>
-						<th>Nombre</th>
-					</td>
-					<td>					
-						<th>Puntos</th>
-					</td>
-				</tr>
-			</tbody>
-		</table>	
-	</div>	
+<div class="rankingM" id="contenido">
+	<div class="container">
+		<div class="row">
+			<div class=" col-xs-12 col-md-10 col-md-offset-1 padding-left">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+	    				<h1 class="panel-title">Ranking Masculino</h1>
+	  				</div>
+	  				<div class="panel-body">
+	  					<div class="table-responsive">
+	  						<table class="table">
+								<thead>
+									<tr>
+										<th style="width: 216px">N°</th>
+										<th>Nombre</th>
+										<th style="width: 216px">Puntos</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1</td>
+										<td>Daniel</td>
+										<td>10000</td>
+									</tr>
+									<tr>
+										<td>2</td>
+										<td>Cristian</td>
+										<td>2000</td>
+									</tr>
+								</tbody>
+							</table>
+	  					</div>
+					</div>
+				</div>	
+			</div>
+		</div>
+	</div>
+</div>
