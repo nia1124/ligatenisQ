36259fab9e7091f37f39ecf47f98736dc7e3e803
nia1124 <nia-5@hotmail.com>
2015-05-16 14:46:55 -0500

    no se

---
+++ vistas/admin.php
@@ -0,0 +1,11 @@
+<div class="admin" id="contenido">
+	<div class="container">
+       <div class="row">
+       	    <div class=" col-xs-12 col-md-10 col-md-offset-1 padding-left">
+       	    	
+       	    </div>
+       </div>
+
+		
+	</div>
+</div>		
